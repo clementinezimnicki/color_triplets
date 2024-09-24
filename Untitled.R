6fbd466c89476eae0b29cf6ab2956f0e0b9cfc27
Clementine Zimnicki <zimnicki@wisc.edu>
2024-09-24 13:51:41 -0500

    updated button

--- Untitled.R
+++ Untitled.R
@@ -1,2 +1,8 @@
+#install.packages("rjson")
+library("rjson")
 
-filenames <- list.files("temp", pattern="*.png", full.names=TRUE)
+setwd("~/Documents/GitHub/color_triplets")
+filenames <- list.files("img", pattern="*.png", full.names=TRUE)
+filenames2 <- as.vector(filenames)
+filenames2 <- toJSON(filenames2)
+write(filenames2, "test.json")
